102b15b7a47cacaa028c6ba2eee8ccab6b3dd117
Leonardo Acosta Ortega <leon_acor@hotmail.com>
2019-10-21 14:03:21 -0500

    cambio de usuarios
    aqui debes de poner las credenciales de tu base de datos

--- bsns/DataSystem.php
+++ bsns/DataSystem.php
@@ -2,11 +2,8 @@
 class DataSystem
 {
   const dbHost = 'localhost';
-  /*const dbUser = 'domestic_udbTeAs';
-  const dbName = 'domestic_dbTeAs';
-  const dbPwd = '7*;tT4*Dm~H{D(?>YCL>?^3L^-LEO';*/
-  const dbUser = 'bsnetcom_dbTeAs';
-  const dbPwd = 'a3h-H6"H76/VY-n4FAky8)jzm';
-  const dbName = 'bsnetcom_dbTeAs';
+  const dbUser = 'dbuser';
+  const dbPwd = 'dbpwd';
+  const dbName = 'dbname';
 }
 ?>
